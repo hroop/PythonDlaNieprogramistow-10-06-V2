9572bad747dad3eafe0144c1f1c5b44734b8f34a
cscomarch <pawelaszczak@gmail.com>
2024-06-11 09:36:43 +0200

    nowe6

--- typy_danych_2.py
+++ typy_danych_2.py
@@ -24,7 +24,7 @@ print(len(lista))
 print(lista[-1])
 print(lista[-2])
 
-#wypisac fragment
+# wypisac fragment
 
 print(lista[0:3])
 print(lista[:3])
@@ -36,4 +36,48 @@ print(lista[2:3])
 print(lista[2:10])
 print(lista[7:10])
 print(lista[2:2])
-print(lista[0:3:2]) #start:stop:krok
+print(lista[0:3:2])  # start:stop:krok
+
+lista_10 = list(range(15))
+print(lista_10)
+print(lista_10[-5:])
+
+# nadpisanie elememtu
+lista[2] = "mikolaj"
+print(lista)
+
+# wstawic element we wskazanie miejsce
+lista.insert(1, "marek")
+print(lista)
+lista.insert(11, "zenek")
+print(lista)
+
+# sprawzdenie indeksu
+print(lista.index("zenek"))
+indeks = lista.index("zenek")
+print(indeks)
+
+# usuwanie elementu list
+lista.remove("zenek")
+print(lista)
+
+lista.append("radek")
+print(lista)
+lista.remove("radek")
+print(lista)
+
+print("radek" in lista)  # True
+
+#usuniecie elementu po indeksie
+indeks = lista.index("mikolaj")
+lista.pop(indeks)
+print(lista)
+print(lista.pop(0))
+print(lista.pop()) #usuwa ostatni element z listy
+print(lista)
+lista.append("Anna")
+lista.append("Tomek")
+lista.append("Paulina")
+print(lista)
+print(lista.pop(-2))
+print(lista)
